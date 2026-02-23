7cd32fdc928492adc3fd5d314b1f08fd86186c19
Ishan Bhardwaj <ishanbhardwaj177@gmail.com>
2026-02-24 00:08:10 +0530

    made get and delete comment api

--- Backend/src/controllers/comment.controller.js
+++ Backend/src/controllers/comment.controller.js
@@ -40,8 +40,49 @@ async function createComment(req, res) {
 
     }
 }
-async function getCommentsByPost(req, res) { }
-async function deleteComment(req, res) { }
+async function getCommentsByPost(req, res) {
+    try {
+        const postId = req.param.id;
+
+        const post = (await postModel.find({ post: postId }).populate("user", "name email")).sort({ createAt: -1 });
+        res.status(200).json(comments);
+
+    } catch (error) {
+        console.error(error);
+        return res.status().json({
+            message: "internal Server error"
+        })
+    }
+}
+async function deleteComment(req, res) {
+    try {
+        const commentID = req.params.id;
+        const comment = await commentModel.findById(commentID);
+        if (!comment) {
+            return res.status(404).json({
+                message: "No comment post"
+            })
+        }
+
+        if (comment.user.toString !== req.user.id) {
+            return res.status(403).json({
+                message: "you are not allowed to delete this post"
+            });
+        }
+
+        await comment.deleteOne();
+
+        res.status(200).json({
+            message: "Comment deleted successfully"
+        });
+
+    } catch (error) {
+        console.error(error);
+        return res.status().json({
+            message: "Inter server Error"
+        })
+    }
+}
 
 module.exports = {
     createComment,
